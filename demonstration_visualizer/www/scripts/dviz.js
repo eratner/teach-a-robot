00161f470c0f0a05a388ff8b7b3be32a87cb83f7
Ellis Ratner <eratner@bowdoin.edu>
2013-08-17 11:49:10 -0400

    Added a basic web-based client for dviz.

---
+++ demonstration_visualizer/www/scripts/dviz.js
@@ -0,0 +1,29 @@
+function init() {
+  var ros = new ROSLIB.Ros({
+    url : 'ws://localhost:9090'
+  });
+
+  var viewer = new ROS3D.Viewer({
+    divID : 'dviz',
+    width : 800,
+    height : 600,
+    antialias : true
+  });
+
+  var tfClient = new ROSLIB.TFClient({
+    ros : ros,
+    angularThres : 0.01,
+    transThres : 0.01,
+    rate : 10.0,
+    fixedFrame : 'map'
+  });
+
+  var imClient = new ROS3D.InteractiveMarkerClient({
+    ros : ros,
+    tfClient : tfClient,
+    topic : '/dviz_interactive_markers',
+    path : 'http://resources.robotwebtools.org/',
+    camera : viewer.camera,
+    rootObject : viewer.selectableObjects
+  });
+}
